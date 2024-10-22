06c8b7c6d69983f6a3d07cd2ecafed564889b898
NetAcad DEVASC <changeme@example.com>
2024-10-22 09:05:49 +0000

    mapquest_parse-json_4.py

--- mapquest_parse-json_4.py
+++ mapquest_parse-json_4.py
@@ -10,7 +10,12 @@ key = os.getenv("MAPQUEST_API_KEY")
 
 while True:
     orig = input("Starting Location: ")
+    if orig == "quit" or orig == "q":
+            break
     dest = input("Destination: ")
+    if dest == "quit" or dest == "q":
+        break
+
     url = main_api + urllib.parse.urlencode({"key": key, "from": orig, "to": dest})
     print("URL: " + url)
     json_data = requests.get(url).json()
