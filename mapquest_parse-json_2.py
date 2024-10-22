46a465d740eccf45adeb0978905d9ff3815a8552
Eksaayl <144189773+Eksaayl@users.noreply.github.com>
2024-10-22 16:32:10 +0800

    Step 5

--- mapquest_parse-json_2.py
+++ mapquest_parse-json_2.py
@@ -6,11 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
-orig = "Rome, Italy"
-dest = "Frascati, Italy"
-key = os.getenv("MAPQUEST_API_KEY")  
+orig = "Washington, D.C."
+dest = "Baltimore, Md"
+key = os.getenv("MAPQUEST_API_KEY") 
 
 url = main_api + urllib.parse.urlencode({"key": key, "from": orig, "to": dest})
 
+print("URL: " + url)
+
 json_data = requests.get(url).json()
-print(json_data)
+
+json_status = json_data["info"]["statuscode"]
+
+if json_status == 0:
+    print("API Status: " + str(json_status) + " = A successful route call.\n")
